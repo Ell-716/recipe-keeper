b41bcc1cdcfa0f59434cad9971a4bd69cc66e2de
Elena <elenabai.2021@gmail.com>
2026-02-08 19:41:12 +0300

    Add JavaScript references and recipes array

---
+++ script.js
@@ -0,0 +1,9 @@
+// Reference HTML Elements
+let recipeForm = document.querySelector('form');
+let recipeName = document.getElementById('recipeName');
+let ingredients = document.getElementById('ingredients');
+let steps = document.getElementById('steps');
+let displayArea = document.getElementById('recipeDisplay');
+
+// Array for Recipes
+let recipes = [];
