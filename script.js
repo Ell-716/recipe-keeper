2512b29169e0e6b2f0f66e5f60a482b6015105a9
Elena <elenabai.2021@gmail.com>
2026-02-08 19:47:48 +0300

    Add recipe submission functionality and form clearing

--- script.js
+++ script.js
@@ -7,3 +7,29 @@ let displayArea = document.getElementById('recipeDisplay');
 
 // Array for Recipes
 let recipes = [];
+
+// Set Up the Event Listener
+recipeForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    // Capture Input Values
+    let enteredRecipeName = recipeName.value;
+    let enteredIngredients = ingredients.value;
+    let enteredSteps = steps.value;
+
+    // create a new recipe
+    let newRecipe = {
+        name: enteredRecipeName,
+        ingredients: enteredIngredients,
+        steps: enteredSteps
+    };
+
+    // add the new recipe to the recipes array
+    recipes.push(newRecipe);
+
+    // Clear the Input Fields
+    recipeName.value = '';
+    ingredients.value = '';
+    steps.value = '';
+
+});
